8d25eabc170ce3d7f198ca7ea3fd8af6eca0cf54
Will Merrill <willm@allenai.org>
2025-09-19 20:16:45 +0000

    Debug hybrid

--- src/olmo_core/nn/transformer/config.py
+++ src/olmo_core/nn/transformer/config.py
@@ -286,9 +286,9 @@ class TransformerBlockConfig(ModuleConfig):
                 # TODO: Also abstract callback for different allocation strategies
                 if block_idx % 2 == 0:
                     kwargs.pop("fla")
-                    # return ReorderedNormTransformerBlock(**kwargs)
+                    return ReorderedNormTransformerBlock(**kwargs)
                     # It looks like the type is in fact default???
-                    return TransformerBlock(**kwargs)
+                    # return TransformerBlock(**kwargs)
                 else:
                     n_heads = self.attention.n_heads
                     kwargs.pop("attention")
